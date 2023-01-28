07c8b76f6df8495402c04e0610b482222e3760f5
Afo <obaone01@gmail.com>
2023-01-28 11:53:43 -0800

    updated components

--- packages/client/src/components/Loader.tsx
+++ packages/client/src/components/Loader.tsx
@@ -1,4 +1,4 @@
-import {} from "react";
+import React from "react";
 
 interface LoaderProps {
   error?: string;
--- packages/client/src/components/Nav.tsx
+++ packages/client/src/components/Nav.tsx
@@ -1,4 +1,5 @@
-import {} from "react";
+import React from "react";
+
 import { ConnectWallet } from "./ConnectWallet";
 
 export const Nav: React.FC = () => {
