40edb5d79a1496c8efe4c4e3903d95560ae3e350
Naama Paulemont <60440166+junaama@users.noreply.github.com>
2023-01-28 20:14:06 -0500

    fix typo

--- packages/contracts/src/systems/InitSystem.sol
+++ packages/contracts/src/systems/InitSystem.sol
@@ -4,7 +4,7 @@ import { System, IWorld } from "solecs/System.sol";
 import { getAddressById } from "solecs/utils.sol";
 import { LibInit } from "libraries/LibInit.sol";
 
-uint256 constant ID = uint256(keccak256("system.Increment"));
+uint256 constant ID = uint256(keccak256("mudSnap.system.Init"));
 
 contract InitSystem is System {
   constructor(IWorld _world, address _components) System(_world, _components) {}
@@ -12,9 +12,5 @@ contract InitSystem is System {
   function execute(bytes memory) public returns (bytes memory) {
     uint256 playerEntity = LibInit.initPlayer(msg.sender);
     LibInit.init(world, components, playerEntity);
-  }
-
-  function executeTyped(uint256 entity) public returns (bytes memory) {
-    return execute(abi.encode(entity));
   }
 }
