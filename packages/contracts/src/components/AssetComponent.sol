5b56733950bd9f672a5886373dc1fbe2a7a8f0d5
Naama Paulemont <60440166+junaama@users.noreply.github.com>
2023-01-28 19:19:31 -0500

    update asset

--- packages/contracts/src/components/AssetComponent.sol
+++ packages/contracts/src/components/AssetComponent.sol
@@ -1,18 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 import "std-contracts/components/StringComponent.sol";
-import { AssetType } from "libraries/MSTypes.sol";
 
 uint256 constant ID = uint256(keccak256("mudSnap.component.Asset"));
 
 contract AssetComponent is StringComponent {
   constructor(address world) StringComponent(world, ID) {}
-
-  function set(uint256 entity, AssetType memory value) public {
-    set(entity, abi.encode(value));
-  }
-
-  function getValueTyped(uint256 entity) public view returns (AssetType memory) {
-    return abi.decode(getRawValue(entity), (AssetType));
-  }
 }
