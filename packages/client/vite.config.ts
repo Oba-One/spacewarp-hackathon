ec040ce59f909bf45572730b4dc76b0ba8211948
Afo <obaone01@gmail.com>
2023-02-01 12:15:26 -0800

    updated configs

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   server: {
     port: 3000,
     fs: {
