0a52042a355d9928f8cfd16a01210645553439f4
Naama <60440166+junaama@users.noreply.github.com>
2023-01-31 19:14:36 -0800

    fix: deploy locally (#9)
    * remove unused
    
    * make fixes

--- packages/contracts/src/components/AssetComponent.sol
+++ packages/contracts/src/components/AssetComponent.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0;
 import "std-contracts/components/StringComponent.sol";
 
-uint256 constant ID = uint256(keccak256("mudSnap.component.Asset"));
+uint256 constant ID = uint256(keccak256("component.Asset"));
 
 contract AssetComponent is StringComponent {
   constructor(address world) StringComponent(world, ID) {}
