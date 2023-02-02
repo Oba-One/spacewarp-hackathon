5d46c129fa76ba938123b4b3162c82ee1de33a52
mehdi <rhouzm@gmail.com>
2023-02-03 00:43:05 +0100

    v1 - init system updated

--- packages/contracts/src/components/MatchComponent.sol
+++ packages/contracts/src/components/MatchComponent.sol
@@ -3,43 +3,44 @@ pragma solidity >=0.8.0;
 
 import "solecs/Component.sol";
 
-import { GodID } from "../libraries/MSTypes.sol";
-uint256 constant ID = uint256(keccak256("component.MatchType"));
+import {GodID, MatchType} from "../libraries/MSTypes.sol";
 
-import { MatchType } from "../libraries/MSTypes.sol";
+uint256 constant _ID = uint256(0x060D);
+uint256 constant ID = uint256(keccak256("component.MatchType"));
 
 ///@notice You can deploy this contract as ID for a system
 contract MatchComponent is Component {
-  constructor(address world) Component(world, ID) {}
+    constructor(address world) Component(world, ID) {}
 
-  function getSchema() public pure override returns (string[] memory keys, LibTypes.SchemaValue[] memory values) {
-    keys = new string[](2);
-    values = new LibTypes.SchemaValue[](2);
+    function getSchema() public pure override returns (string[] memory keys, LibTypes.SchemaValue[] memory values) {
+        keys = new string[](3);
+        values = new LibTypes.SchemaValue[](3);
 
-    keys[0] = "gameBegin";
-    values[0] = LibTypes.SchemaValue.UINT256;
+        keys[0] = "startedAt";
+        values[0] = LibTypes.SchemaValue.UINT256;
 
-    keys[1] = "moveCardLength";
-    values[1] = LibTypes.SchemaValue.UINT32;
-  }
+        keys[1] = "finishedAt";
+        values[1] = LibTypes.SchemaValue.UINT256;
 
-  function set(uint256 entity, MatchType calldata config) public {
-    set(entity, encodedValue(config));
-  }
+        key[2] = "turnsLeft";
+        values[2] = LibTypes.SchemaValue.UINT32;
+    }
 
-  function getValue(uint256 entity) public view returns (MatchType memory) {
-    (uint256 startedAt, uint256 finishedAt, uint8 turnsLeft, uint32 moveCardLength) = abi.decode(
-      getRawValue(entity),
-      (uint256, uint256, uint8, uint32)
-    );
-    return MatchType(startedAt, finishedAt, turnsLeft, moveCardLength);
-  }
+    function set(uint256 entity, MatchType calldata config) public {
+        set(entity, encodedValue(config));
+    }
 
-  function getEntitiesWithValue(MatchType calldata config) public view returns (uint256[] memory) {
-    return getEntitiesWithValue(encodedValue(config));
-  }
+    function getValue(uint256 entity) public view returns (MatchType memory) {
+        (uint256 startedAt, uint256 finishedAt, uint8 turnsLeft, uint32 moveCardLength) =
+            abi.decode(getRawValue(entity), (uint256, uint256, uint8, uint32));
+        return MatchType(startedAt, finishedAt, turnsLeft, moveCardLength);
+    }
 
-  function encodedValue(MatchType calldata config) private pure returns (bytes memory) {
-    return abi.encode(config.startedAt, config.moveCardLength);
-  }
+    function getEntitiesWithValue(MatchType calldata config) public view returns (uint256[] memory) {
+        return getEntitiesWithValue(encodedValue(config));
+    }
+
+    function encodedValue(MatchType calldata config) private pure returns (bytes memory) {
+        return abi.encode(config.startedAt, config.moveCardLength);
+    }
 }
--- packages/contracts/src/systems/InitSystem.sol
+++ packages/contracts/src/systems/InitSystem.sol
@@ -1,36 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
-import { System, IWorld } from "solecs/System.sol";
-import { getAddressById } from "solecs/utils.sol";
-import { LibInit } from "libraries/LibInit.sol";
-import { IdentityType, PositionEnum } from "libraries/MSTypes.sol";
-import { OwnedByComponent, ID as OwnedByID } from "../components/OwnedByComponent.sol";
-import { IdentityComponent, ID as IdentityID } from "../components/IdentityComponent.sol";
-import { AssetComponent, ID as AssetID } from "../components/AssetComponent.sol";
-import { PositionComponent, ID as PositionID } from "../components/PositionComponent.sol";
+
+import {System, IWorld} from "solecs/System.sol";
+import {getAddressById} from "solecs/utils.sol";
+import {LibInit} from "libraries/LibInit.sol";
+import {IdentityType, PositionEnum, MatchType} from "libraries/MSTypes.sol";
+import {OwnedByComponent, ID as OwnedByID} from "../components/OwnedByComponent.sol";
+import {IdentityComponent, ID as IdentityID} from "../components/IdentityComponent.sol";
+import {AssetComponent, ID as AssetID} from "../components/AssetComponent.sol";
+import {PositionComponent, ID as PositionID} from "../components/PositionComponent.sol";
+import {MatchComponent, ID as MatchID} from "../components/MatchComponent.sol";
 
 uint256 constant ID = uint256(keccak256("system.Init"));
 
 contract InitSystem is System {
-  constructor(IWorld _world, address _components) System(_world, _components) {}
+    constructor(IWorld _world, address _components) System(_world, _components) {}
 
-  function execute(bytes memory) public override returns (bytes memory) {
-    uint256 playerEntity = LibInit.initPlayer(msg.sender);
-    IdentityType memory identity1 = IdentityType({ name: "SpiderMan1", description: "Spiderman One" });
-    // @junaama TODO: remove hardcoding later
-    string
-      memory asset = "https://images.unsplash.com/photo-1635805737707-575885ab0820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+    function execute(bytes memory) public override returns (bytes memory) {
+        uint256 playerEntity = LibInit.initPlayer(msg.sender);
+        IdentityType memory identity1 = IdentityType({name: "SpiderMan1", description: "Spiderman One"});
+        // @junaama TODO: remove hardcoding later
+        string memory asset =
+            "https://images.unsplash.com/photo-1635805737707-575885ab0820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
 
-    PositionEnum position = PositionEnum.Deck;
-    uint256 characterEntity = world.getUniqueEntityId();
+        PositionEnum position = PositionEnum.Deck;
+        uint256 characterEntity = world.getUniqueEntityId();
 
-    PositionComponent(getAddressById(components, PositionID)).set(characterEntity, position);
-    OwnedByComponent(getAddressById(components, OwnedByID)).set(characterEntity, playerEntity);
-    AssetComponent(getAddressById(components, AssetID)).set(characterEntity, asset);
-    IdentityComponent(getAddressById(components, IdentityID)).set(characterEntity, identity1);
-  }
+        PositionComponent(getAddressById(components, PositionID)).set(characterEntity, position);
+        OwnedByComponent(getAddressById(components, OwnedByID)).set(characterEntity, playerEntity);
+        AssetComponent(getAddressById(components, AssetID)).set(characterEntity, asset);
+        IdentityComponent(getAddressById(components, IdentityID)).set(characterEntity, identity1);
+        matchComponent MatchComponent = MatchComponent(getAddressById(components, MatchComponentID));
 
-  function executeTyped() public returns (bytes memory) {
-    return execute(new bytes(0));
-  }
+        matchComponent.set(_ID, MatchType({startedAt: block.timestamp, finishedAt: block.timestamp, turnsLeft: 5}));
+    }
+
+    function executeTyped() public returns (bytes memory) {
+        return execute(new bytes(0));
+    }
 }
