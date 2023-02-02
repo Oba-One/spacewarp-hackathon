dae7efe2caae734dce060f1c7801930a79c62292
Afo <obaone01@gmail.com>
2023-02-01 19:52:15 -0800

    updated player

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,15 +1,11 @@
 import { SWRConfig } from "swr";
-import { useState } from "react";
 import { WagmiConfig } from "wagmi";
-import { LivepeerConfig } from "@livepeer/react";
 import { HuddleClientProvider } from "@huddle01/huddle01-client";
 import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 
 import Game from "./views/Game";
 
-import { Nav } from "./components/Nav";
-import { Loader } from "./components/Loader";
-import { huddleClient, livepeerClient, wagmiClient } from "modules/clients";
+import { huddleClient, wagmiClient } from "./modules/clients";
 
 const Views = () => {
   return (
@@ -21,15 +17,6 @@ const Views = () => {
 };
 
 function App() {
-  const [status, setStatus] = useState<"loading" | "done" | "error">("done");
-  const [error, setError] = useState("");
-
-  function handleReload() {
-    setStatus("loading");
-    window.location.reload();
-    setError("");
-  }
-
   return (
     <WagmiConfig client={wagmiClient}>
       <SWRConfig
@@ -37,15 +24,11 @@ function App() {
           refreshInterval: 3000,
         }}
       >
-          <HuddleClientProvider value={huddleClient}>
-            <BrowserRouter>
-              {(status === "loading" || status === "error") && (
-                <Loader error={error} reload={handleReload} />
-              )}
-              <Nav />
-              <Views />
-            </BrowserRouter>
-          </HuddleClientProvider>
+        <HuddleClientProvider value={huddleClient}>
+          <BrowserRouter>
+            <Views />
+          </BrowserRouter>
+        </HuddleClientProvider>
       </SWRConfig>
     </WagmiConfig>
   );
--- packages/client/src/views/Game/index.tsx
+++ packages/client/src/views/Game/index.tsx
@@ -1,63 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { LivepeerConfig } from "@livepeer/react";
+import { useRootStore } from "@huddle01/huddle01-client";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
-import { livepeerClient } from "modules/clients";
-import { Player } from "components/Game/Player";
-import { useRootStore } from "@huddle01/huddle01-client/*";
+import { livepeerClient } from "../../modules/clients";
+import { Player } from "../../components/Player";
 
 /**
  * Game View
  * 1, Will render unity view here and once the game is loaded, remove loader
  */
 const Game: React.FC = () => {
-  const { unityProvider, sendMessage, addEventListener, removeEventListener } =
-    useUnityContext({
-      dataUrl: "unity/game.data",
-      frameworkUrl: "unity/game.framework.js",
-      loaderUrl: "unity/game.loader.js",
-      codeUrl: "unity/game.wasm",
-      companyName: "MudSnap",
-      productName: "MudSnap",
-      productVersion: "0.1",
-    });
-
-  const peerId = useRootStore((state) => state.peers[0].peerId);
-
-  const onBtnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    sendMessage(
-      "MainCamera",
-      "ReactToUnityMethod",
-      JSON.stringify({
-        foo: "bar",
-      })
-    );
-  };
-
-  useEffect(() => {
-    const cb = (params: any) => {
-      console.log("DemoUnityToReact", params);
-    };
-
-    addEventListener("DemoUnityToReact", cb);
-
-    return () => {
-      removeEventListener("DemoUnityToReact", cb);
-    };
+  const { unityProvider } = useUnityContext({
+    dataUrl: "unity/game.data",
+    frameworkUrl: "unity/game.framework.js",
+    loaderUrl: "unity/game.loader.js",
+    codeUrl: "unity/game.wasm",
+    companyName: "MudSnap",
+    productName: "MudSnap",
+    productVersion: "0.1",
   });
+
+  const [gameCode, setGameCode] = useState(1);
+
+  const peerId = useRootStore((state) =>
+    state.peers.length ? state.peers[0].peerId : ""
+  );
 
   return (
     <LivepeerConfig client={livepeerClient}>
-      <Unity
-        className="unity"
-        unityProvider={unityProvider}
-        style={{ width: "100vw", height: "90vh" }}
-      />
-      <section id="react" className="fixed inset-0 w-screen h-screen ">
-        <Player type="opponent" peerId={peerId} gameId="" />
-        <Player type="player" peerId={peerId} gameId="" />
-        {/* <button onClick={onBtnClick}>Click</button> */}
+      <section
+        id="react"
+        className="fixed z-10 flex h-screen w-screen justify-between"
+      >
+        <Player
+          type="opponent"
+          peerId={peerId}
+          gameCode={gameCode}
+          setGameCode={setGameCode}
+        />
+        <Player
+          type="player"
+          peerId={peerId}
+          gameCode={gameCode}
+          setGameCode={setGameCode}
+          opponentId={peerId}
+        />
+      </section>
+      <section
+        id="unity"
+        className="flex grid h-screen w-screen place-items-center"
+      >
+        <Unity className="h-screen w-screen" unityProvider={unityProvider} />
       </section>
     </LivepeerConfig>
   );
