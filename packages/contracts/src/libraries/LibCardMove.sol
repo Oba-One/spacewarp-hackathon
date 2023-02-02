963ed8157b58392e0a9c9cfb71d40a074ed64bba
Afo <obaone01@gmail.com>
2023-02-01 20:12:12 -0800

    fixed spme errors

--- packages/contracts/src/libraries/LibCardMove.sol
+++ packages/contracts/src/libraries/LibCardMove.sol
@@ -2,20 +2,21 @@
 pragma solidity >=0.8.0;
 
 //External
-import {getAddressById} from "solecs/utils.sol";
-import {IUint256Component} from "solecs/interfaces/IUint256Component.sol";
+import { getAddressById } from "solecs/utils.sol";
+import { IUint256Component } from "solecs/interfaces/IUint256Component.sol";
 
 //Components
-import {PositionComponent, ID as PositionComponentID} from "../components/PositionComponent.sol";
-import {Uint32Component} from "std-contracts/components/Uint32Component.sol";
+import { PositionComponent, ID as PositionComponentID } from "../components/PositionComponent.sol";
+import { Uint32Component } from "std-contracts/components/Uint32Component.sol";
 
 // Types
-import {MoveCard} from "./MSTypes.sol";
+import { MoveCard } from "./MSTypes.sol";
 
 /**
  * @notice
  * @param
- * @return
  */
 
-library LibCardMove {}
+library LibCardMove {
+
+}
