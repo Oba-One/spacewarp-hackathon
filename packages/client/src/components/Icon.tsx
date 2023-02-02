f84419fae4442af0d0333a6faf71bc7ccd44d9c1
Afo <obaone01@gmail.com>
2023-02-01 19:51:49 -0800

    updated components

--- packages/client/src/components/Icon.tsx
+++ packages/client/src/components/Icon.tsx
@@ -6,18 +6,22 @@ interface IconProps {
   Svg: React.FC<React.SVGProps<SVGSVGElement>>;
   size?: number;
   color?: string;
+  active?: boolean;
   listItem?: boolean;
   className?: string;
 }
 
+export const iconStyles =
+  "relative h-12 w-12 m-1 before:absolute before:rounded-full beforehover:bg-slate-700";
+
 export const Icon: FC<IconProps> = ({ title, Svg, size, color, listItem }) => {
   if (listItem) {
-    <li className="grid place-items-center">
+    <li className={`${iconStyles} grid place-items-center`}>
       <Tooltip title={title} position="bottom" html={<Svg />} />
     </li>;
   }
   return (
-    <div>
+    <div className={`${iconStyles} grid place-items-center`}>
       <Tooltip title={title} position="bottom" html={<Svg />} />
     </div>
   );
--- packages/client/src/components/Input.tsx
+++ packages/client/src/components/Input.tsx
@@ -1,17 +1,20 @@
 import { forwardRef } from "react";
+import { Tooltip } from "react-tippy";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  icon?: { Svg: React.FC<React.SVGProps<SVGSVGElement>>; title: string };
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ id, placeholder, ...props }, ref) => (
+  ({ id, placeholder, label, icon, ...props }, ref) => (
     <label
       htmlFor={id}
       className="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
     >
       <input
         {...props}
+        id={id}
         ref={ref}
         placeholder={placeholder}
         className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
@@ -19,6 +22,21 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       <span className="absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs">
         {placeholder}
       </span>
+      {icon && (
+        <span className="absolute inset-y-0 right-0 grid w-10 place-content-center">
+          <Tooltip
+            title={icon.title}
+            html={
+              <button
+                type="button"
+                className="rounded-full bg-rose-600 p-0.5 text-white hover:bg-rose-700"
+              >
+                <icon.Svg />
+              </button>
+            }
+          />
+        </span>
+      )}
     </label>
   )
 );
