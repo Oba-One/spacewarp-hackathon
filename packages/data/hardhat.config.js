7625c5462f9c25f941744c6926e37a15cd3c30a1
Afo <obaone01@gmail.com>
2023-02-02 02:30:25 -0800

    added basic dao contract

--- packages/data/hardhat.config.js
+++ packages/data/hardhat.config.js
@@ -4,7 +4,7 @@ require("hardhat-deploy-ethers")
 require("./tasks")
 require("dotenv").config()
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY ?? ""
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.17",
