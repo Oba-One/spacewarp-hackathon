a7ed1a82e041a5f59ff688942486215806ba1925
mehdi <rhouzm@gmail.com>
2023-02-03 01:21:25 +0100

    v1 MoveSystem added + libraries

--- packages/contracts/src/libraries/MSTypes.sol
+++ packages/contracts/src/libraries/MSTypes.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import "std-contracts/components/CoordComponent.sol";
+
 uint256 constant snapID = uint256(0x060D);
 
 enum ElementEnum {
