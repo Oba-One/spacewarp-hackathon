c29a3c735f14f3091ab463082f77b6be0673aa16
Afo <obaone01@gmail.com>
2023-02-03 15:20:11 -0800

    work baby

--- packages/client/src/components/Nav.tsx
+++ packages/client/src/components/Nav.tsx
@@ -4,8 +4,9 @@ import { ConnectWallet } from "./ConnectWallet";
 
 export const Nav: React.FC = () => {
   return (
-    <nav className="header py-02 sticky top-0 flex items-center justify-between bg-white px-8 shadow-md">
+    <header className="header py-02 sticky top-0 flex items-center justify-between bg-white px-8 shadow-md">
+      <img src='' alt='Dao Smack Logo' className='' />
       <ConnectWallet />
-    </nav>
+    </header>
   );
 };
