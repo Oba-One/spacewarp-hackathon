109ff06f1d785a45c7f3da302f081a75cd90e5e6
Naama Paulemont <60440166+junaama@users.noreply.github.com>
2023-02-04 12:16:40 -0500

    refactor merkles

--- packages/data/src/League.sol
+++ packages/data/src/League.sol
@@ -6,29 +6,30 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract League {
-
- event SquadCreated(address newAddress, uint256 squadId);
+    event SquadCreated(address newAddress, uint256 squadId);
     bytes public matchMerkleRoot;
+    bytes public playerMerkleRoot;
+    bytes public winnerMerkleRoot;
 
     mapping(uint256 => address) public squads;
-   modifier notInOtherSquad(){
-    for(uint i = 0; i < _squads.length; i++){
-      Squad squad = _squads[i];
-      require(squad.isMember(msg.sender) == false, "Already in a squad");
+    modifier notInOtherSquad(){
+      for(uint i = 0; i < _squads.length; i++){
+        Squad squad = _squads[i];
+        require(squad.isMember(msg.sender) == false, "Already in a squad");
+      }
+      _;
     }
-    _;
-  }
 
     modifier isValidMerkleProof(bytes32[] calldata merkleProof, bytes32 root) {
-        require(
-            MerkleProof.verify(
-                merkleProof,
-                root,
-                keccak256(abi.encodePacked(msg.sender))
-            ),
-            "Address does not exist in list"
-        );
-        _;
+      require(
+          MerkleProof.verify(
+              merkleProof,
+              root,
+              keccak256(abi.encodePacked(msg.sender))
+          ),
+          "Address does not exist in list"
+      );
+      _;
     }
 
   function createSquad() public returns (uint256){
@@ -48,29 +49,28 @@ contract League {
     Squad squad = Squad(squads[_index]);
     squad.join();
   }
-    function isMember(address member) public view returns (bool) {
-        for (uint i = 0; i < _squads.length; i++) {
-            Squad squad = _squads[i];
-            if (squad.isMember(member)) {
-                return true;
-            }
+  function isMember(address member) public view returns (bool) {
+    for (uint i = 0; i < _squads.length; i++) {
+        Squad squad = _squads[i];
+        if (squad.isMember(member)) {
+            return true;
         }
-        return false;
     }
+    return false;
+  }
     function squadMembers(uint256 _squadId) public view returns (address[] memory) {
         Squad squad = Squad(squads[_squadId]);
         return squad.members();
     }
-    function validPlayerInMatch(uint256 matchId, address player) isValidMerkleProof {
 
-    }
-    function validMatchGame(uint256 matchId) isValidMerkleProof {
-
-    }
-    function validMatchWinner(uint256 matchId, address player) isValidMerkleProof {
-
-    }
     function setMatchMerkleRoot(bytes memory _matchMerkleRoot) external onlyOwner {
         matchMerkleRoot = _matchMerkleRoot;
     }
+    function setWinnerMerkleRoot(bytes memory _winnerMerkleRoot) external onlyOwner {
+        winnerMerkleRoot = _winnerMerkleRoot;
+    }
+    function setPlayerMerkleRoot(bytes memory _playerMerkleRoot) external onlyOwner {
+        playerMerkleRoot = _playerMerkleRoot;
+    }
+
 }
