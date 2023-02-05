72d34e27d918dcdc06796ac34549447e6c7c025d
Afo <obaone01@gmail.com>
2023-02-05 02:59:36 -0800

    nav updates

--- packages/client/src/components/Nav.tsx
+++ packages/client/src/components/Nav.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import { avatarGenerator } from "../utils/avatarGenerator";
 import { ConnectWallet } from "./ConnectWallet";
 
 export const Nav: React.FC = () => {
   return (
-    <header className="navbar flex items-center justify-between bg-gray-900 px-8">
-      <img src="" alt="Dao Smack Logo" className="" />
-      <nav className="flex items-center justify-center"></nav>
-      <ConnectWallet />
+    <header className="navbar flex items-center justify-between bg-gray-900 px-8 py-3">
+      <div className="navbar-start">
+        <img
+          src={avatarGenerator.generateRandomAvatar()}
+          alt="Dao Smack Logo"
+          className="h-12 w-12"
+        />
+      </div>
+      <div className="navbar-center">
+        <Link to="/play" className="btn-ghost btn text-2xl normal-case">
+          Play
+        </Link>
+        <Link to="/squad" className="btn-ghost btn text-2xl normal-case">
+          Squad
+        </Link>
+      </div>
+      <div className="navbar-end">
+        <ConnectWallet />
+      </div>
     </header>
   );
 };
--- packages/client/src/utils/avatarGenerator.ts
+++ packages/client/src/utils/avatarGenerator.ts
@@ -1,9 +1,9 @@
 import { AvatarGenerator } from "random-avatar-generator";
 
-export const generator = new AvatarGenerator();
+export const avatarGenerator = new AvatarGenerator();
 
 // Simply get a random avatar
-export const playerAvatar = generator.generateRandomAvatar();
+export const playerAvatar = avatarGenerator.generateRandomAvatar();
 
 // Optionally specify a seed for the avatar. e.g. for always getting the same avatar for a user id.
 // With seed 'avatar', always returns https://avataaars.io/?accessoriesType=Kurt&avatarStyle=Circle&clotheColor=Blue01&clotheType=Hoodie&eyeType=EyeRoll&eyebrowType=RaisedExcitedNatural&facialHairColor=Blonde&facialHairType=BeardMagestic&hairColor=Black&hatColor=White&mouthType=Sad&skinColor=Yellow&topType=ShortHairShortWaved
