77b5095ba0710763dd6096f3560c6dd3d26e08d7
Mehdi <75360886+mrhouzlane@users.noreply.github.com>
2023-02-06 07:20:57 +0100

    V1 (#28)
    * v1 - init system updated
    
    * fix : v1 set params
    
    * v1 MoveSystem added + libraries
    
    * test v0
    
    * ok
    
    * forge install: forge-std
    
    v1.3.0
    
    * add
    
    * forge install: openzeppelin-contracts
    
    v4.8.1
    
    * fix : compiling contracts
    
    * update
    
    * added constants for decks
    
    * v1 - init system updated
    
    * fix : v1 set params
    
    * v1 MoveSystem added + libraries
    
    * test v0
    
    * forge install: forge-std
    
    v1.3.0
    
    * add
    
    * feat : added systems + PrevAct Components
    
    * fix : MoveSystem adding new smart contracts
    
    ---------
    
    Co-authored-by: Afo <obaone01@gmail.com>

--- packages/contracts/script/MudSnap3.sol
+++ packages/contracts/script/MudSnap3.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.11;
 import "forge-std/Script.sol";
 
 contract MyScript is Script {
-    function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
+  function run() external {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(deployerPrivateKey);
 
-        vm.stopBroadcast();
-    }
+    vm.stopBroadcast();
+  }
 }
--- packages/contracts/types/ethers-contracts/factories/index.ts
+++ packages/contracts/types/ethers-contracts/factories/index.ts
@@ -4,7 +4,14 @@
 export { ActionSystem__factory } from "./ActionSystem__factory";
 export { AssetComponent__factory } from "./AssetComponent__factory";
 export { BareComponent__factory } from "./BareComponent__factory";
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
 export { CommitSystem__factory } from "./CommitSystem__factory";
+>>>>>>> b8b513cdbb0d1bb2ca65acf7faba59802bb83286
+=======
+export { CommitSystem__factory } from "./CommitSystem__factory";
+>>>>>>> 658be4eb8f03f20a856785f96e3b5e1db79bc2c6
 export { CommitmentComponent__factory } from "./CommitmentComponent__factory";
 export { CoordComponent__factory } from "./CoordComponent__factory";
 export { EffectComponent__factory } from "./EffectComponent__factory";
@@ -17,8 +24,12 @@ export { InGameComponent__factory } from "./InGameComponent__factory";
 export { InitSystem__factory } from "./InitSystem__factory";
 export { LibQuery__factory } from "./LibQuery__factory";
 export { MatchComponent__factory } from "./MatchComponent__factory";
+<<<<<<< HEAD
+export { MoveCardComponent__factory } from "./MoveCardComponent__factory";
+=======
 export { MoveCharacterSystem__factory } from "./MoveCharacterSystem__factory";
 export { MoveSystem__factory } from "./MoveSystem__factory";
+>>>>>>> 658be4eb8f03f20a856785f96e3b5e1db79bc2c6
 export { OwnedByComponent__factory } from "./OwnedByComponent__factory";
 export { PhaseComponent__factory } from "./PhaseComponent__factory";
 export { PositionComponent__factory } from "./PositionComponent__factory";
